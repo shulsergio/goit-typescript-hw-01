c81d69375af8822ebb81efc8047c7a0fd3ab13ef
Sergey Shulga <shul.sergio@gmail.com>
2024-10-06 21:59:17 +0300

    v. 1.0 start ts

---
+++ src/basic/1.ts
@@ -0,0 +1,11 @@
+// const age = 50;
+// const username = 'Max';
+// const toggle = true;
+// const empty = null;
+// const callback = (a) => { return 100 + a };
+
+// const age = 50;
+// const username = 'Max';
+// const toggle = true;
+// const empty = null;
+// const callback = (a) => { return 100 + a };
