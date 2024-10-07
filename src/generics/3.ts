fb7653d0afa71900cbb79d4690f527e3b86b5343
Sergey Shulga <shul.sergio@gmail.com>
2024-10-07 20:14:05 +0300

    v 1.3 fix err

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -3,6 +3,6 @@
 // }
 
 
-function merge(objA: object, objB: object): object {
+function merge < T extends object, U extends object> (objA: T, objB: U): T & U {
   return Object.assign(objA, objB);
 }
